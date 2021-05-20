f87c670e836f927eeb0d376da0850e9980e242e2
minkukjo <strike0115@naver.com>
2021-05-20 22:45:54 +0900

    DB JPA Property

--- src/main/java/me/harry/iam/IamApplication.java
+++ src/main/java/me/harry/iam/IamApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IamApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(IamApplication.class, args);
     }
-
 }
