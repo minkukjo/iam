ba058dfec2db092d743a9b6296424c2a7a5e71ed
minkukjo <strike0115@naver.com>
2021-05-22 02:36:33 +0900

    테스트 코드 한글 깨짐 이슈 수정

--- src/test/java/me/harry/iam/AbstractMvcTest.java
+++ src/test/java/me/harry/iam/AbstractMvcTest.java
@@ -1,16 +1,34 @@
 package me.harry.iam;
 
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @SpringBootTest
-@AutoConfigureMockMvc
 public abstract class AbstractMvcTest {
 
-    @Autowired
     protected MockMvc mockMvc;
 
+    @Autowired
+    private WebApplicationContext ctx;
+
+    @BeforeEach
+    void setup() {
+        this.mockMvc = MockMvcBuilders
+                .webAppContextSetup(ctx)
+                .addFilters(new CharacterEncodingFilter("UTF-8",true))
+                .alwaysDo(print())
+                .build();
+    }
 }
