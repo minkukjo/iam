2055ba37d7100496d294ec3dd979c1ddff99a04b
minkukjo <strike0115@naver.com>
2021-05-24 16:10:36 +0900

    CGLib 사용에 따른 인터페이스 제거

--- src/main/java/me/harry/iam/application/board/BoardService.java
+++ src/main/java/me/harry/iam/application/board/BoardService.java
@@ -1,11 +1,35 @@
 package me.harry.iam.application.board;
 
+import lombok.AllArgsConstructor;
 import me.harry.iam.domain.board.Post;
+import me.harry.iam.infrastructure.PostRepository;
 import me.harry.iam.presentation.dto.PostDTO;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface BoardService {
-    Post writePost(PostDTO postDTO);
-    Post readPost();
-    Post editPost();
-    void deletePost();
+@Service
+@AllArgsConstructor
+public class BoardService {
+    private final PostRepository postRepository;
+
+    @Transactional
+    public Post writePost(PostDTO postDTO) {
+        Post post = Post.builder()
+                .title(postDTO.getTitle())
+                .content(postDTO.getContent())
+                .build();
+        return postRepository.save(post);
+    }
+
+    public Post readPost() {
+        return null;
+    }
+
+    public Post editPost() {
+        return null;
+    }
+
+    public void deletePost() {
+
+    }
 }
--- src/main/java/me/harry/iam/presentation/BoardController.java
+++ src/main/java/me/harry/iam/presentation/BoardController.java
@@ -4,7 +4,6 @@ import me.harry.iam.application.board.BoardService;
 import me.harry.iam.presentation.dto.PostDTO;
 import me.harry.iam.presentation.response.PostResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
