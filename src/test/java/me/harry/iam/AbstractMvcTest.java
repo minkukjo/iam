eae250ad17f0b82f4121cf4a0d0fe2fe2343e69d
minkukjo <strike0115@naver.com>
2021-05-30 16:15:21 +0900

    Query DSL 코드 작성

--- src/test/java/me/harry/iam/AbstractMvcTest.java
+++ src/test/java/me/harry/iam/AbstractMvcTest.java
@@ -1,20 +1,19 @@
 package me.harry.iam;
 
-import org.aspectj.lang.annotation.Before;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
+@Rollback
+@Transactional
 @SpringBootTest
 public abstract class AbstractMvcTest {
 
@@ -27,7 +26,7 @@ public abstract class AbstractMvcTest {
     void setup() {
         this.mockMvc = MockMvcBuilders
                 .webAppContextSetup(ctx)
-                .addFilters(new CharacterEncodingFilter("UTF-8",true))
+                .addFilters(new CharacterEncodingFilter("UTF-8", true))
                 .alwaysDo(print())
                 .build();
     }
--- src/test/java/me/harry/iam/board/BoardTest.java
+++ src/test/java/me/harry/iam/board/BoardTest.java
@@ -1,17 +1,21 @@
 package me.harry.iam.board;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import me.harry.iam.AbstractMvcTest;
+import me.harry.iam.domain.board.Post;
+import me.harry.iam.domain.board.QPost;
 import me.harry.iam.presentation.dto.PostDTO;
 import me.harry.iam.utils.JacksonUtil;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -36,5 +40,30 @@ public class BoardTest extends AbstractMvcTest {
                 .andDo(print())
                 .andExpect(status().is(HttpStatus.OK.value()))
                 .andExpect(jsonPath("$.title").exists());
+    }
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    @DisplayName("Query DSL 테스트")
+    @Transactional
+    public void queryDSL() {
+        Post build = Post.builder()
+                .title("hello")
+                .content("my")
+                .build();
+
+        em.persist(build);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QPost post = QPost.post;
+
+        Post post1 = query
+                .selectFrom(post)
+                .fetchOne();
+
+        assertEquals(post1, build);
+
     }
 }
